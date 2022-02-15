5682574386d4abe6b7a5343e3d5d0adc232c7e51
kibet hillary <kibethillaire@gmail.com>
2022-02-15 22:36:51 +0300

    Added Files

---
+++ Fundamentals/list_comprehension.py
@@ -0,0 +1,59 @@
+# EXAMPLE
+numbers = [0, 1, 2, 3, 4, 5]
+doubled_numbers = []
+
+for number in numbers:
+    doubled_numbers.append(number * 2)
+
+print(doubled_numbers)
+
+# LIST COMPREHENSION 1
+numbers = [0, 1, 2, 3, 4, 5]
+doubled_numbers = [number * 2 for number in numbers]
+
+print(doubled_numbers)
+
+
+# LIST COMPREHENSION 2
+doubled_numbers = [number * 2 for number in range(6)]
+
+print(doubled_numbers)
+
+
+# LIST COMPREHENSION 3
+friend_ages = [22, 34, 19, 56, 37]
+age_strings = [f'my friend is {age} years old.' for age in friend_ages]
+
+print(age_strings)
+
+
+# LIST COMPREHENSION 4
+names = ["Rolf", "Kibet", "Jen"]
+lower = [name.lower() for name in names]
+
+print(lower)
+
+
+
+# LIST COMPREHENSION 5
+friend = input("Enter your friends name: ")
+friends = ["Rolf", "Kibet", "Jen"]
+friends_lower = [name.lower() for name in friends]
+
+if friend.lower() in friends_lower:
+    print(f"{friend} is one of your friends")
+
+if friend.lower() in friends_lower:
+    friend_titlecased = friend.title()
+    print(f"{friend_titlecased} is one of your friends")
+
+
+# LIST COMPREHENSION 6
+friend = input("Enter your friends name: ")
+friends = ["Rolf", "Kibet", "Jen"]
+friends_lower = [name.lower() for name in friends]
+
+if friend.lower() in friends_lower:
+    friend_titlecased = friend.title()
+    print(f"{friend_titlecased} is one of your friends")
+
---
+++ Fundamentals/list_slicing.py
@@ -0,0 +1,12 @@
+friends = ['kibet', 'Paul', 'Pato', 'Richie', 'anna', 'Rael']
+
+######
+# the first number is the starting point and the second is the ending point
+######
+print(friends[2:4]) # gets friends 2 and 3, excludes 4
+print(friends[1:]) # get all the elements except the first one
+print(friends[:4]) # start from the beginning and exclud 4
+print(friends[:]) # gives you a new list
+print(friends[-3:]) # starts counting from the left -1, -2,-3
+print(friends[:-2]) # count from the beginning upto the 2nd last element
+print(friends[-3:-1])
